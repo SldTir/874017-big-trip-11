98f6128a5744a201c0aecd3cb62a4e1bf4035d41
Sld Tir <pasha.tir1991@mail.ru>
2020-04-02 16:56:20 +0300

    Исправил замечания линтера

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const createRouteInformationTemplate = () => {
   return (
     `<section class="trip-main__trip-info  trip-info">
@@ -385,13 +387,13 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeaderElement = document.querySelector('.page-header');
-const siteTripMain = siteHeaderElement.querySelector('.trip-main');
-const siteTropControl = siteHeaderElement.querySelector('.trip-controls');
-const siteTripControlHeaderMenu = siteTropControl.querySelector('h2:nth-child(1)');
-const siteTripControlHeaderFilter = siteTropControl.querySelector('h2:nth-child(2)');
-const sitePageMainElement = document.querySelector('.page-main');
-const siteTripEventElement = sitePageMainElement.querySelector('.trip-events');
+const siteHeaderElement = document.querySelector(`.page-header`);
+const siteTripMain = siteHeaderElement.querySelector(`.trip-main`);
+const siteTropControl = siteHeaderElement.querySelector(`.trip-controls`);
+const siteTripControlHeaderMenu = siteTropControl.querySelector(`h2:nth-child(1)`);
+const siteTripControlHeaderFilter = siteTropControl.querySelector(`h2:nth-child(2)`);
+const sitePageMainElement = document.querySelector(`.page-main`);
+const siteTripEventElement = sitePageMainElement.querySelector(`.trip-events`);
 
 render(siteTripMain, createRouteInformationTemplate(), `afterBegin`);
 render(siteTripControlHeaderMenu, createSiteMenuTemplate(), `afterEnd`);
